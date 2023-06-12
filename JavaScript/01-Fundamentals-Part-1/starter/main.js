452ea77a79d045fe9ec75592f8dda7c24537a8e6
Dope Karibbean <dopekaribbean@gmail.com>
2023-06-12 21:44:33 +0200

    Add ReadMe file
    - add ReadMe ;
    - coding challenge 4 ;

--- JavaScript/01-Fundamentals-Part-1/starter/main.js
+++ JavaScript/01-Fundamentals-Part-1/starter/main.js
@@ -9,7 +9,7 @@ console.log(typeof javascriptIsFun);*/
 
 
 
-// Coding Challenge #1
+// CODING CHALLENGE #1
 /* Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula :
 BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
 
@@ -63,7 +63,7 @@ console.log(bmiDope);*/
 
 
 
-// Coding Challenge #2
+// CODING CHALLENGE #2
 /* Use the BMI example from Challenge #1, and the code you already wrote, and
 improve it.
 Your tasks:
@@ -82,7 +82,7 @@ GOOD LUCK 😀 */
 
 
 
-//Coding Challenge #3
+// CODING CHALLENGE #3
 /* There are two gymnastics teams, Dolphins and Koalas. They compete against each
 other 3 times. The winner with the highest average score wins a trophy!*/
 
@@ -101,12 +101,12 @@ other 3 times. The winner with the highest average score wins a trophy!*/
 GOOD LUCK 😀 */
 
 // Average calcul
-const averageDolphins = (96 + 108 + 89) / 3
+/*const averageDolphins = (96 + 108 + 89) / 3
 const averageKoalas = (88 + 91 + 110) / 3
-console.log(`The Dolphin average is ${averageDolphins} and the Koala average is ${averageKoalas}`);
+console.log(`The Dolphin average is ${averageDolphins} and the Koala average is ${averageKoalas}`);*/
 
 // Comparison average
-if (averageDolphins > averageKoalas) {
+/*if (averageDolphins > averageKoalas) {
    console.log(`The winner of the competition is the Dolphins with an average ${averageDolphins}`);
 } else if (averageDolphins < averageKoalas) {
    console.log(`The winner of the competition is the Koalas with an average ${averageKoalas}`);
@@ -114,10 +114,10 @@ if (averageDolphins > averageKoalas) {
    console.log(`It's a draw : ${averageDolphins}, ${averageKoalas}. No team wins the trophy`);
 } else {
    console.log(`Error !`)
-}
+}*/
 
 // Bonus 1 :
-const bonusAverageDolphins = (97 + 112 + 101) / 3
+/*const bonusAverageDolphins = (97 + 112 + 101) / 3
 const bonusAverageKoalas = (109 + 95 + 123) / 3
 console.log(`The Dolphin average is ${bonusAverageDolphins} and the Koala average is ${bonusAverageKoalas}`);
 
@@ -129,10 +129,10 @@ if (bonusAverageDolphins >= 100 && bonusAverageDolphins > bonusAverageKoalas) {
    console.log(`It's a draw : ${bonusAverageDolphins}, ${bonusAverageKoalas}. No team wins the trophy`);
 } else {
    console.log(`Error !`);
-}
+}*/
 
 // Bonus 2 :
-const averageDolphinsBonus = (97 + 112 + 101) / 3
+/*const averageDolphinsBonus = (97 + 112 + 101) / 3
 const averageKoalasBonus = (109 + 95 + 106) / 3
 console.log(`The Dolphin average is ${bonusAverageDolphins} and the Koala average is ${bonusAverageKoalas}`);
 
@@ -144,4 +144,42 @@ if (averageDolphinsBonus >= 100 && averageDolphinsBonus > averageKoalasBonus) {
    console.log(`It's a draw : Dolphins average is ${averageDolphinsBonus} and Koalas avarage is ${averageDolphinsBonus}. No team wins the trophy`);
 } else {
    console.log(`Error !`);
-}
+}*/
+
+
+
+// CODING CHALLENGE #4
+/*Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant.
+In his country, it's usual to tip 15% if the bill value is between 50 and 300.
+If the value is different, the tip is 20%.*/
+
+// Your tasks:
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+
+// 2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+
+// Test data:
+// § Data 1: Test for bill values 275, 40 and 430
+/*Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+GOOD LUCK 😀*/
+
+// Task 1 :
+const bill = 275;
+const bill1 = 40;
+const bill2 = 430;
+
+// ternary
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2 ;
+
+const tip1 = bill1 >= 50 && bill1 <= 300 ? bill1 * 0.15 : bill1 * 0.2 ;
+
+const tip2 = bill2 >= 50 && bill2 <= 300 ? bill2 * 0.15 : bill2 * 0.2 ;
+
+// Task 2 :
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+console.log(`The bill was ${bill1}, the tip was ${tip1}, and the total value ${bill1 + tip1}`);
+
+console.log(`The bill was ${bill2}, the tip was ${tip2}, and the total value ${bill2 + tip2}`);
--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# My JavaScript Journey 🎒
+# My Fullstack Journey 🎒
+
+![Fullstack Journey](preview/readMeBanner.png)
+
+
+## Table of contents
+
+- [🗺️ The roadmap](#the-roadmap)
+- [🧠 Courses](#courses)
+- [📅 The calendar](#the-calendar)
+- [🔗 Links](#links)
+- [📦 Useful resources](#useful-resources)
+- [😎 Author](#author)
+
+
+## 🗺️ The roadmap
+- [🧠 Courses](#courses)
+- [📅 The calendar](#the-calendar)
+- [🔗 Links](#links)
+- [📦 Useful resources](#useful-resources)
+- [😎 Author](#author)
+
+
+## 🗺️ The roadmap
+
+- ✔️ ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+
+- ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+
+- ![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+
+- ![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
+
+- ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+
+- ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
+
+- ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+- ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+
+- ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white)
+
+- ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+
+
+- ![Threejs](https://img.shields.io/badge/threejs-black?style=for-the-badge&logo=three.js&logoColor=white)
+
+- ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+- ![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white)
+
+## 🧠 Courses
+
+- [Complete Javascript course](https://www.udemy.com/course/the-complete-javascript-course/)
+
+- [NodeJs - MongoDb - Express course](https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/)
+
+
+## 📅 The calendar
+
+📆 12/06/2023
+- Difference between expressions and statements ;
+- The conditional Ternary operator ;
+- Brief history of JavaScript ;
